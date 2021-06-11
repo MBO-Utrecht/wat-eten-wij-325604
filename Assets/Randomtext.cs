d24735745fcb4ac113acb05fe99f29c43abab119
rick letschert <ricktest@letschert.com>
2021-06-11 11:33:38 +0200

    huiswerk

---
+++ Assets/Randomtext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Randomtext : MonoBehaviour
+{
+    public Text _textobject;
+    public string[] _randomtext;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+        Randomize();
+
+        
+         void Randomize()
+        {
+            _textobject.text = _randomtext[Random.Range(0, 10)];
+        }
+    }
+}
